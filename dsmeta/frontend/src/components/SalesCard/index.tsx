43755e6b19739b7459b454a08722731d65ee154e
Gabriel Batista <gabriel_batista@trimble.com>
2022-11-18 15:13:42 -0300

    (feat) implementing date range filter

--- dsmeta/frontend/src/components/SalesCard/index.tsx
+++ dsmeta/frontend/src/components/SalesCard/index.tsx
@@ -16,11 +16,14 @@ function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([])
 
   useEffect(()=>{
-    axios.get(`${BASE_URL}/sales`)
+    const minSearchDate = minDate.toISOString().slice(0, 10)
+    const mixSearchDate = maxDate.toISOString().slice(0, 10)
+
+    axios.get(`${BASE_URL}/sales?minDate=${minSearchDate}&maxDate=${mixSearchDate}`)
       .then(response => {
         setSales(response.data.content)
       })
-  }, []) 
+  }, [minDate, maxDate]) 
 
   return (
       <div className="dsmeta-card">
@@ -77,19 +80,6 @@ function SalesCard() {
                 )
               })
             }
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
           </tbody>
 
         </table>
